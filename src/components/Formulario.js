30d855240523c2e29260dd0ba90d3f17426431d2
Adrian <adrianfernandezj@gmail.com>
2020-05-27 14:32:58 -0300

    131-Pasando criptomonedas desde la API

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 
-import useMoneda from '../hooks/useMoneda'
-import useCriptomoneda from '../hooks/useCriptomoneda'
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
 
 const Boton = styled.input`
     margin-top: 20px;
@@ -24,6 +25,9 @@ const Boton = styled.input`
 
 const Formulario = () => {
     
+    //State de listado de criptomonedas
+    const [listacripto, guardarCriptomonedas] = useState([]);
+
     const MONEDAS = [
         {codigo: 'USD', nombre: 'Dólar de USA'},
         {codigo: 'MXN', nombre: 'Peso Mexicano'},
@@ -36,6 +40,16 @@ const Formulario = () => {
     
     // Utilizar useCriptomoneda
     const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '');
+
+    // Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI= async() => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
     
     return ( 
         <form>
